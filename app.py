1d1a4ae6cd6769590f2153faa6cd0fee8d18a99f
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2023-02-21 13:42:21 +0000

    added database

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
 
-JOBS =[{
-  'id': 1,
-  'question': 'Who made is c?'
-},
-      {
-        'id': 2,
-        'question': 'Who made python?'
-      }]
+class Product(db.Model):
+  name = db.Column(db.String(30), nullable=False, unique=True, primary_key=True)
+  price = db.Column(db.Integer(), nullable=False)
+
+  def __init__(self, name, price):
+    self.name = name
+    self.price = price
+
+  
+with app.app_context():
+    db.create_all()
+
 @app.route("/")
 def hello():
-  return render_template('home.html', jobs=JOBS)
+  product = Product.query.all()
+  return render_template('home.html', product=product)
 
 
 if __name__ == "__main__":
